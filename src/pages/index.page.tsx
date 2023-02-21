f453091598806c9c84219189fb311a6bdd2a2061
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2023-02-21 14:35:26 +0100

    Add layout folder, component, footer, nav, page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
-}
+import Head from "next/head";
+import { Page } from "@/components/layout/Page";
+
+export const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Explorerz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Page>
+          <h1>Welcome</h1>
+        </Page>
+      </main>
+    </>
+  );
+};
+
+export default Home;
