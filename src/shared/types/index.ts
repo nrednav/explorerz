7cbde3b5c1be87011ccac9d3295f33a89cdedae9
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2023-02-22 11:44:11 +0100

    Refactor tile

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const TileSchema = z.object({
+export const TileSchema = z.object({
   id: z.number(),
   kind: z.enum(["grass", "water", "sand", "stone"]),
   image: z.object({
