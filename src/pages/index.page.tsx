86ece034e16af4d17308b06455cf5f8e643ce816
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2023-02-24 15:21:22 +0100

    Add Inventory panel

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Head from "next/head";
 import Error from "@/components/data-display/Error";
+import InventoryPanel from "@/components/data-display/InventoryPanel";
 import Loading from "@/components/data-display/Loading";
 import { Map } from "@/components/data-display/Map";
+import Button from "@/components/input-and-actions/Button";
 import useMap from "@/hooks/useMap";
 
 export const Home = () => {
   const { data: tiles, isLoading, isError } = useMap();
+  const [openInventory, setOpenInventory] = useState(true);
+
+  const InventoryPanelOnClick = () => setOpenInventory(!openInventory);
 
   if (isError) return <Error message="Could not load map..." />;
   if (isLoading) return <Loading />;
@@ -18,6 +24,8 @@ export const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Map tiles={tiles} />
+      <Button onClick={InventoryPanelOnClick} ctaText="Inventory" />
+      <InventoryPanel onClick={InventoryPanelOnClick} open={openInventory} />
     </>
   );
 };
