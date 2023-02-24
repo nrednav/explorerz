08ddeca122d0414d5b85d7a2b110abac16f113e0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2023-02-24 17:44:19 +0100

    Add types

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
+export const TileKindSchema = z.enum(["grass", "water", "sand", "stone"]);
+export type TileKind = z.infer<typeof TileKindSchema>;
+
 export const TileSchema = z.object({
   id: z.number(),
   kind: z.enum(["grass", "water", "sand", "stone"]),
@@ -10,3 +13,12 @@ export type Tile = z.infer<typeof TileSchema>;
 
 export const TileGridSchema = TileSchema.nullable().array().array();
 export type TileGrid = z.infer<typeof TileGridSchema>;
+
+export const TileCollectionSchema = z.object({
+  grass: TileSchema.array(),
+  water: TileSchema.array(),
+  sand: TileSchema.array(),
+  stone: TileSchema.array(),
+});
+
+export type TileCollection = z.infer<typeof TileCollectionSchema>;
