4218c37dd4d2d6ea3141c1d950de582060802eaf
nrednav <nrednav@gmail.com>
2023-02-26 14:22:26 +0000

    update: getMap.ts

--- src/flow/cadence/scripts/getMap.ts
+++ src/flow/cadence/scripts/getMap.ts
@@ -3,9 +3,29 @@ import * as fcl from "@onflow/fcl";
 
 const code = `
 import Cartographer from 0xCartographer
+import NonFungibleToken from 0xNonFungibleToken
 
-pub fun main(): Cartographer.Map {
-    return Cartographer.map
+pub fun main(): [[Cartographer.TileDetails?]] {
+
+    let map: [[Cartographer.TileDetails?]] = []
+
+    for row in Cartographer.map.tiles {
+        let rowOfTileDetails: [Cartographer.TileDetails?] = []
+
+        for tile in row {
+            if tile == nil {
+                rowOfTileDetails.append(nil)
+                continue
+            }
+
+            let tileDetails = Cartographer.getTileDetails(id: tile!.id)
+            rowOfTileDetails.append(tileDetails)
+        }
+
+        map.append(rowOfTileDetails)
+    }
+
+    return map
 }
 `;
 
