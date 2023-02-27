08bb36d50f6b6a5b9879ded7f8ed5b71d48fd6dd
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2023-02-27 13:52:26 +0100

    Style minting phase, add current minting phase

--- src/components/data-display/MintingPhases.tsx
+++ src/components/data-display/MintingPhases.tsx
@@ -41,23 +41,15 @@ const steps = [
 
 const CompleteStep: FC<StepProps> = ({ stepIdx, step }) => {
   return (
-    <div>
-      <span
-        className="absolute top-0 left-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
-        aria-hidden="true"
-      />
+    <div className="bg-slate-500">
       <span
         className={clsx(
           stepIdx !== 0 ? "lg:pl-9" : "",
-          "flex items-start px-6 py-5 text-sm font-medium"
+          "flex justify-around px-6 py-5 text-sm font-medium text-white lg:flex-col lg:gap-y-2"
         )}
       >
-        <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-          <span className="text-sm font-medium">{step.id}</span>
-          <span className="text-sm font-medium text-gray-500">
-            {step.description}
-          </span>
-        </span>
+        <span>{step.id}</span>
+        <span>{step.description}</span>
       </span>
     </div>
   );
@@ -65,23 +57,15 @@ const CompleteStep: FC<StepProps> = ({ stepIdx, step }) => {
 
 const CurrentStep: FC<StepProps> = ({ stepIdx, step }) => {
   return (
-    <div>
-      <span
-        className="absolute top-0 left-0 h-full w-1 bg-blue-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
-        aria-hidden="true"
-      />
+    <div className="bg-blue-400">
       <span
         className={clsx(
           stepIdx !== 0 ? "lg:pl-9" : "",
-          "flex items-start px-6 py-5 text-sm font-medium"
+          "flex justify-around px-6 py-5 text-sm font-medium text-white lg:flex-col lg:gap-y-2"
         )}
       >
-        <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-          <span className="text-sm font-medium text-blue-400">{step.id}</span>
-          <span className="text-sm font-medium text-black">
-            {step.description}
-          </span>
-        </span>
+        <span>{step.id}</span>
+        <span>{step.description}</span>
       </span>
     </div>
   );
@@ -91,21 +75,13 @@ const UpcomingStep: FC<StepProps> = ({ stepIdx, step }) => {
   return (
     <div>
       <span
-        className="absolute top-0 left-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
-        aria-hidden="true"
-      />
-      <span
         className={clsx(
           stepIdx !== 0 ? "lg:pl-9" : "",
-          "flex items-start px-6 py-5 text-sm font-medium"
+          "flex justify-around bg-slate-400 px-6 py-5 text-sm font-medium text-white lg:flex-col lg:gap-y-2"
         )}
       >
-        <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-          <span className="text-sm font-medium text-gray-500">{step.id}</span>
-          <span className="text-sm font-medium text-gray-500">
-            {step.description}
-          </span>
-        </span>
+        <span>{step.id}</span>
+        <span>{step.description}</span>
       </span>
     </div>
   );
@@ -116,23 +92,19 @@ const MintingPhases = () => {
     <div className="mx-auto max-w-[1024px] py-4 lg:py-8">
       <div className="py-4">
         <h2 className="text-center text-2xl font-bold text-gray-900">
-          Minting phases
+          Minting phase:
+          {steps.filter((step) => step.status === StepStatus.current)[0].id}
         </h2>
       </div>
-      <div className="lg:border-t lg:border-b lg:border-gray-200">
-        <nav className="mx-auto max-w-7xl" aria-label="Progress">
-          <ol
-            role="list"
-            className="overflow-hidden rounded-md lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200"
-          >
+      <div className="pixelated w-full bg-slate-600 text-white after:text-slate-800 hover:text-white focus:outline-none">
+        <nav aria-label="Minting phase">
+          <ol role="list" className="overflow-hidden lg:flex">
             {steps.map((step, stepIdx) => (
               <li key={step.id} className="relative overflow-hidden lg:flex-1">
                 <div
                   className={clsx(
-                    stepIdx === 0 ? "rounded-t-md border-b-0" : "",
-                    stepIdx === steps.length - 1
-                      ? "rounded-b-md border-t-0"
-                      : "",
+                    stepIdx === 0 ? "border-b-0" : "",
+                    stepIdx === steps.length - 1 ? "border-t-0" : "",
                     "overflow-hidden border border-gray-200 lg:border-0"
                   )}
                 >
@@ -147,22 +119,9 @@ const MintingPhases = () => {
                     <>
                       {/* Separator */}
                       <div
-                        className="absolute inset-0 top-0 left-0 hidden w-3 lg:block"
+                        className="absolute inset-0 top-0 left-0 hidden w-1 border border-slate-600 bg-slate-600 lg:block"
                         aria-hidden="true"
-                      >
-                        <svg
-                          className="h-full w-full text-gray-300"
-                          viewBox="0 0 12 82"
-                          fill="none"
-                          preserveAspectRatio="none"
-                        >
-                          <path
-                            d="M0.5 0V31L10.5 41L0.5 51V82"
-                            stroke="currentcolor"
-                            vectorEffect="non-scaling-stroke"
-                          />
-                        </svg>
-                      </div>
+                      ></div>
                     </>
                   ) : null}
                 </div>
