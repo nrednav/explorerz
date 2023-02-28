36b8113bdd92500bf82f50e151e526d78f29c281
nrednav <nrednav@gmail.com>
2023-02-28 11:38:48 +0000

    feat: cleanup provision of account addresses to fcl in frontend

--- src/flow/config.ts
+++ src/flow/config.ts
@@ -1,5 +1,10 @@
 import * as fcl from "@onflow/fcl";
 
+const FLOW_NETWORK = process.env.NEXT_PUBLIC_FLOW_NETWORK || "emulator";
+const FLOW_EMULATOR_ACCOUNT =
+  process.env.NEXT_PUBLIC_FLOW_EMULATOR_ACCOUNT || "";
+const FLOW_TESTNET_ACCOUNT = process.env.NEXT_PUBLIC_FLOW_TESTNET_ACCOUNT || "";
+
 type Network = "emulator" | "testnet";
 
 const getConfig = (network: Network) => {
@@ -10,10 +15,10 @@ const getConfig = (network: Network) => {
         "accessNode.api": "http://localhost:8888",
         "flow.network": "emulator",
         "discovery.wallet": "http://localhost:8701/fcl/authn", // Point FCL at dev-wallet (default port)
-        "0xCartographer": "0xf8d6e0586b0a20c7",
-        "0xTileMinter": "0xf8d6e0586b0a20c7",
-        "0xNonFungibleToken": "0xf8d6e0586b0a20c7",
-        "0xMetadataViews": "0xf8d6e0586b0a20c7",
+        "0xCartographer": FLOW_EMULATOR_ACCOUNT,
+        "0xTileMinter": FLOW_EMULATOR_ACCOUNT,
+        "0xNonFungibleToken": FLOW_EMULATOR_ACCOUNT,
+        "0xMetadataViews": FLOW_EMULATOR_ACCOUNT,
       };
     case "testnet":
       return {
@@ -21,14 +26,12 @@ const getConfig = (network: Network) => {
         "accessNode.api": "https://rest-testnet.onflow.org",
         "flow.network": "testnet",
         "discovery.wallet": "https://flow-wallet-testnet.blocto.app/authn",
-        "0xCartographer": "0x7e384d013d79153e",
-        "0xTileMinter": "0x7e384d013d79153e",
+        "0xCartographer": FLOW_TESTNET_ACCOUNT,
+        "0xTileMinter": FLOW_TESTNET_ACCOUNT,
         "0xNonFungibleToken": "0x631e88ae7f1d7c20",
         "0xMetadataViews": "0x631e88ae7f1d7c20",
       };
   }
 };
-
-const FLOW_NETWORK = process.env.NEXT_PUBLIC_FLOW_NETWORK || "emulator";
 
 fcl.config(getConfig(FLOW_NETWORK as Network));
